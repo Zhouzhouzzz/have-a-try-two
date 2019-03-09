cd7baaf3306c94ba42c2601df4fbf7c96c1a17a2
Zhouzhouzzz <48087256+Zhouzhouzzz@users.noreply.github.com>
2019-03-09 23:29:29 +0800

    Add files via upload

---
+++ baidu.js
@@ -0,0 +1,9 @@
+function clickButton(){
+    var ul=document.getElementById("list");
+    ul.style.display="block"
+}
+
+function close(){
+    var ul=document.getElementById("list");
+    ul.style.display="none"
+}
